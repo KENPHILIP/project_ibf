88dc974b69d855813e2a798e12ba26805e80c906
Kenphilip <125455081+KENPHILIP@users.noreply.github.com>
2025-02-27 08:01:08 +0300

    Delete jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++
@@ -1,7 +0,0 @@
-c = get_config()
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 443
-c.NotebookApp.open_browser = False
-c.NotebookApp.allow_remote_access = True
-c.NotebookApp.token = ''  # We'll set this via environment variable
-c.NotebookApp.password = ''  # We'll set this via hashed password
